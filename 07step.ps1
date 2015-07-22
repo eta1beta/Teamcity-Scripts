1d3dc0f04592ff6a6e67feb7d94b55aa7ac858e9
Matteo Di Giorgio <m.digiorgio@apexnet.it>
2015-07-22 12:16:14 +0200

    Modifiche per verifica velocità
    Divisa creazione Application da applicazione permessi a APP_DATA per
    elmah per verificare velocità di esecuzione

--- 07step.ps1
+++ 07step.ps1
@@ -51,6 +51,21 @@ foreach ($app in $apps) {
 	$Acl.AddAccessRule($rule)
 	Set-Acl -path $elmahpath $Acl    
 
+	## New-WebApplication -Name $app `
+    ##                    -ApplicationPool $appPool `
+    ##                    -PhysicalPath "$installationPath\$app" `
+    ##                    -Site $site `
+    ##                    -Verbose
+}
+foreach ($app in $apps) {
+
+	## $elmahpath ="$installationPath\$app\App_data"
+	## New-Item -ItemType Directory -Path "$elmahpath"
+	## $Acl = Get-Acl $elmahpath
+	## $rule = New-Object   System.Security.AccessControl.FileSystemAccessRule("IIS_IUSRS","FullControl","Allow")
+	## $Acl.AddAccessRule($rule)
+	## Set-Acl -path $elmahpath $Acl    
+
 	New-WebApplication -Name $app `
                        -ApplicationPool $appPool `
                        -PhysicalPath "$installationPath\$app" `
