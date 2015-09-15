0432586de02f163a01ab1bcc2e3d668d2b5822d1
Matteo Di Giorgio <m.digiorgio@apexnet.it>
2015-09-15 11:26:35 +0200

    Aggiunto TRIM
    Aggiunto TRIM per limite lunghezza nome Application pool

--- age-branch-cleaner.ps1
+++ age-branch-cleaner.ps1
@@ -1,11 +1,20 @@
 ﻿$Now = Get-Date
-$Days = "20"
+$Days = "15"
 $TargetFolder = "C:\inetpub\agenda-branch"
 $LastWrite = $Now.AddDays(-$Days)
+
+function Trim-Length {
+param (
+    [parameter(Mandatory=$True,ValueFromPipeline=$True)] [string] $Str
+  , [parameter(Mandatory=$True,Position=1)] [int] $Length
+)
+    $Str[0..($Length-1)] -join ""
+}
 
 $ROOT_DIRECTORY = "C:\inetpub\agenda-branch"
 $branchName = "%teamcity.build.branch%"
 $normalizedBranchName = $branchName.replace('/', '-')
+$normalizedBranchName = $normalizedBranchName | Trim-Length 60
 
 $appPool = "$normalizedBranchName"
 
@@ -50,16 +59,16 @@ foreach ($Folder in $Folders)
     foreach ($app in $apps)
     {
         Write-output "Deleting app $app" >> $Logfilename
-        # Remove-WebApplication -Name $app -Site $site -ErrorAction Ignore
+        Remove-WebApplication -Name $app -Site $site -ErrorAction Ignore
     }
     $appPool = $Folder.name
     
     # REMOVE EACH APPLICATION POOL OF THE FOLDER-BRANCH TO DELETE
-    Write-output "Deleting app pool  $appPool"  >> $Logfilename
-    # Remove-WebAppPool -Name $appPool -ErrorAction Ignore
+    Write-output "Deleting app pool  age-$appPool"  >> $Logfilename
+    Remove-WebAppPool -Name age-$appPool -ErrorAction Ignore
     
     # REMOVE FOLDER
     write-output "Deleting folder $TargetFolder\$Folder" >> $Logfilename
     Write-output "----------------------------------------------" >> $Logfilename
-    # Remove-Item $TargetFolder\$Folder -force -recurse -Confirm:$false -ErrorAction Ignore
+    Remove-Item $TargetFolder\$Folder -force -recurse -Confirm:$false -ErrorAction Ignore
 }
