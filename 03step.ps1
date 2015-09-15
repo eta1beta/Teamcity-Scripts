cb7929ea3dcb1e7ef99d5cd026e12bfb38937f9b
Matteo Di Giorgio <m.digiorgio@apexnet.it>
2015-09-15 11:04:17 +0200

    Aggiunto TRIM
    Trim su nome cartella e quindi nome application pool

--- 03step.ps1
+++ 03step.ps1
@@ -1,9 +1,18 @@
 import-module webadministration
 
+function Trim-Length {
+param (
+    [parameter(Mandatory=$True,ValueFromPipeline=$True)] [string] $Str
+  , [parameter(Mandatory=$True,Position=1)] [int] $Length
+)
+    $Str[0..($Length-1)] -join ""
+}
+
 $ROOT_DIRECTORY = "C:\inetpub\agenda-branch"
 
 $branchName = "%teamcity.build.branch%"
 $normalizedBranchName = $branchName.replace('/', '-')
+$normalizedBranchName = $normalizedBranchName | Trim-Length 60
 
 $site = "Default Web site\age-branch\$normalizedBranchName"
 $appPool = "age-$normalizedBranchName"
